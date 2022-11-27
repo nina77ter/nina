294204a09816e8f9bf0d6461fdd65eb915f2da0f
HP101 <ter77person@gmail.com>
2022-11-27 18:43:23 -0500

    feature 1 scenario 3.4 added

--- feture_automation.py
+++ feture_automation.py
@@ -1,4 +1,10 @@
+
+import time
 print('Automation feature1')
 print('scenario 1 is working... ')
+time.sleep(2)
 print('scenario 2 is working... ')
+
+print('scenario 3 is working... ')
+print('scenario 4 is working... ')
 print('feature 1 testing completed')
